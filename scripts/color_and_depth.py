214fa3ed6cdc5ca4a3d046fd73c116a52a15f99d
YugoNishio <s20c1089dk@s.chibakoudai.jp>
2021-12-02 20:18:13 +0900

    画像と深度

--- scripts/color_and_depth.py
+++ scripts/color_and_depth.py
@@ -10,12 +10,11 @@ from sensor_msgs import msg
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-
 class ImageConvert:
     def __init__(self):
         self.bridge = CvBridge()
-        self.subscribed_image_color = rospy.Subscriber("/camera/color/image_raw", Image, self.color_callback_and_convert)
-        #self.subscribed_image_depth = rospy.Subscriber("/camera/depth/image_rect_war", Image, self.color_callback_and_convert)
+        #self.subscribed_image_color = rospy.Subscriber("/camera/color/image_raw", Image, self.color_callback_and_convert)
+        self.subscribed_image_depth = rospy.Subscriber("/camera/depth/image_rect_raw", Image, self.depth_callback_and_convert)
 
     def main(self):
         try:
@@ -24,35 +23,28 @@ class ImageConvert:
             print("aaaa")
         cv2.destroyAllWindows()
 
-    def color_callback_and_convert(self, msg):
+    def color_callback_and_convert(self, topic):
         try:
-            cv_image_color = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-            #cv_image_depth = self.bridge.imgmsg_to_cv2(msg, "32FC1")
-
+            cv_image_color = self.bridge.imgmsg_to_cv2(topic, "rgb8")
+#            cv_image_depth = self.bridge.imgmsg_to_cv2(topic, "32FC1")
         except CvBridgeError as e:
             print(e)
-
-        rospy.loginfo('subscribed_iimage_color')
-
-        #images = np.hstack((cv_image_color, cv_image_depth))
-
-        #cv2.imshow("Origin Image", images)
-        cv2.imshow("Origin Image", cv_image_color)
-
-        #cv2.imshow("depth image", cv_image_depth)
-        cv2.waitKey(3)
-    
+        rospy.loginfo('subscribed_image_color')
+        self.show(cv_image_color)
+        
     def depth_callback_and_convert(self, msg):
         try:
             cv_image_depth = self.bridge.imgmsg_to_cv2(msg, "32FC1")
         except CvBridgeError as e:
             print(e)
-        
-        cv2.imshow("depth image", cv_image_depth)
+        rospy.loginfo('subscribed_image_depth')
+        self.show(cv_image_depth)
+
+    def show(self, image):
+        cv2.imshow("image", image)
         cv2.waitKey(3)
 
 if __name__ == '__main__':
     rospy.init_node('subscribed_image_color')
     image_convert = ImageConvert()
-    sub = rospy.Subscriber("/bottle_color", Image, image_convert.main)
     image_convert.main()
